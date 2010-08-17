f197ce81ca5ec1c12260eab668aee24c5cca5cf5
John Prince <jtprince@gmail.com>
2010-08-17 13:39:45 -0600

    updated README

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,27 +2,9 @@
 require 'rubygems'
 require 'spec/more'
 
-# This is already defined in our module
-#TESTFILES = File.expand_path(File.dirname(__FILE__)) + '/testfiles'
-
 Bacon.summary_on_exit
 
-#module Bacon
-#  class Context
-#    def hash_match(hash, obj)
-#      hash.each do |k,v|
-#        if v.is_a?(Hash)
-#          hash_match(v, obj.send(k.to_sym))
-#        else
-#          puts "#{k}: #{v} but was #{obj.send(k.to_sym)}" if obj.send(k.to_sym) != v
-#          obj.send(k.to_sym).should.equal v
-#        end
-#      end
-#    end
-#  end
-#end
-
-
+# is this already defined??
 TESTFILES = File.expand_path(File.dirname(__FILE__)) + "/testfiles"
 
 begin
